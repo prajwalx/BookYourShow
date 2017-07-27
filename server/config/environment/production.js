064010a0fcf360b5c2728e90fa2b81cf9dcbf114
prajwalx <prajwalbhati@hotmail.com>
2017-07-27 18:30:45 +0530

    Hope Babel Works

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -19,7 +19,7 @@ module.exports = {
           process.env.MONGOHQ_URL ||
           process.env.OPENSHIFT_MONGODB_DB_URL +
           process.env.OPENSHIFT_APP_NAME ||
-          'mongodb://prajwal:prajwal28@ds125183.mlab.com:25183/yomanprojectfolder-dev'
+          'mongodb://prajwal:prajwal28@ds125183.mlab.com:25183/yomanprojectfolder'
           // 'mongodb://localhost/yomanprojectfolder'
   }
 };
--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://prajwal:prajwal28@ds125183.mlab.com:25183/yomanprojectfolder-dev'
+    uri: 'mongodb://prajwal:prajwal28@ds125183.mlab.com:25183/yomanprojectfolder-test'
     // 'mongodb://localhost/yomanprojectfolder-test'
   },
   sequelize: {
