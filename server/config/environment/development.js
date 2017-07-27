290243a5987bf599c91b9299fee181edee10434b
prajwalx <prajwalbhati@hotmail.com>
2017-07-27 18:14:28 +0530

    Hope Babel Works

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -6,7 +6,8 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/yomanprojectfolder-dev'
+    uri: 'mongodb://prajwal:prajwal28@ds125183.mlab.com:25183/yomanprojectfolder-dev'
+    // 'mongodb://localhost/yomanprojectfolder-dev'
   },
 
   // Seed database on startup
--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,7 +5,8 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/yomanprojectfolder-test'
+    uri: 'mongodb://prajwal:prajwal28@ds125183.mlab.com:25183/yomanprojectfolder-dev'
+    // 'mongodb://localhost/yomanprojectfolder-test'
   },
   sequelize: {
     uri: 'sqlite://',
